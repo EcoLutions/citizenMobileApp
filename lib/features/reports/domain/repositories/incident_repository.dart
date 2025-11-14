20b576dadaeb8fcc52ab8770dccb3fde69460e03
elmer-riva <elmeraugustoriva@gmail.com>
2025-11-13 23:04:01 -0500

    feat: add IncidentRepository interface

--- lib/features/reports/domain/repositories/incident_repository.dart
+++ lib/features/reports/domain/repositories/incident_repository.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:citizen_mobile_app/features/reports/domain/entities/incident_report.dart';
+import 'package:citizen_mobile_app/features/reports/data/model/evidence_resource.dart';
+import 'package:citizen_mobile_app/features/reports/data/model/report_resource.dart';
 
 abstract class IncidentRepository {
-  Future<bool> submitIncident(IncidentReport report);
+  Future<ReportResource> submitIncident(IncidentReport report);
+  Future<EvidenceResource> uploadEvidence(File file, {String? description});
 }
